a8f30a16995843a020f2ab9ca23ca755bcfd7ae1
TomasGM <tomasgonzalezm99@gmail.com>
2023-05-09 23:45:54 -0300

    Home development

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Configuración de directorio público
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Ruta para la página principal
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'home.html'));
+});
+
+app.get('/register', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'register.html'));
+});
+
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'login.html'));
+});
+
+// Inicio del servidor
+app.listen(PORT, () => {
+  console.log('Servidor en el puerto '+PORT);
+});
