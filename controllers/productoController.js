0940d2e18cc4bda631a49f895b996825902fca34
FacundoPougy <facu.pougy@gmail.com>
2023-05-22 16:47:46 -0300

    partials y mejoras visuales en el detalle del producto

--- controllers/productoController.js
+++ controllers/productoController.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 const controller = {
   getDetalleDelProducto: (req, res) => {
-    res.sendFile(path.join(__dirname, "../views/product-detail.ejs"));
+    res.render("product-detail.ejs");
   },
 };
 
