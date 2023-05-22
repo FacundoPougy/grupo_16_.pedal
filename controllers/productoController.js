34d3ecc4c8479b6594eb5263ff435ad505244460
FacundoPougy <facu.pougy@gmail.com>
2023-05-22 19:50:10 -0300

    creando y empezando la tienda de productos

--- controllers/productoController.js
+++ controllers/productoController.js
@@ -4,6 +4,9 @@ const controller = {
   getDetalleDelProducto: (req, res) => {
     res.render("product-detail.ejs");
   },
+  getProductos: (req, res) => {
+    res.render("products");
+  },
 };
 
 module.exports = controller;
--- routes/productoRoutes.js
+++ routes/productoRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const productoController = require("../controllers/productoController.js");
 const router = express.Router();
 
+router.get("/", productoController.getProductos);
 router.get("/detalle-del-producto", productoController.getDetalleDelProducto);
 
 module.exports = router;
