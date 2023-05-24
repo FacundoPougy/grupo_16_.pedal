9f5465056566944cd7bda0584f8310a811e1d58d
jaider3050 <espanajaider@gmail.com>
2023-05-23 21:45:03 -0500

    ciertos commits al register y login

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -2,9 +2,7 @@ const path = require("path");
 
 const controller = {
   getLogin: (req, res) => {
-    res.sendFile(
-      path.join(__dirname, "../views/login.ejs")
-    ); /* falta que existe el html de login, pero esta bien linkeado */
+    res.render('login', { title: 'Login' });
   },
 };
 
