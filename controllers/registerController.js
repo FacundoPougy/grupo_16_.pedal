d452699ece0be3e4bcd8088dcf5d3394d0ba0db2
TomasGM <tomasgonzalezm99@gmail.com>
2023-05-25 20:04:03 -0300

    Register, mejoras

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -1,10 +1,9 @@
-const path = require("path");
 
 const controller = {
   getRegister: (req, res) => {
-    res.sendFile(
-      path.join(__dirname, "../views/register.ejs")
-    ); /* falta que existe el html de register, pero esta bien linkeado */
+    res.render('register', {
+      title: 'Register'
+    });
   },
 };
 
