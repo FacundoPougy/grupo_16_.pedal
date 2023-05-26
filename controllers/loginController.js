384332677fec106c8243d3c9f980a4b1b6b951e3
jaider3050 <espanajaider@gmail.com>
2023-05-26 15:14:03 -0500

    Pusheando css y ejs del login

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 const controller = {
   getLogin: (req, res) => {
-    res.render('login',{ title: '.pedal' });
+    res.render('login',{ title: 'Login' });
   },
 };
 
