097bdfd9735261b13009dc48f6ce6db9a2b833ca
FacundoPougy <facu.pougy@gmail.com>
2023-07-05 22:27:55 -0300

    Multer para el registro de usuarios

--- routes/registerRoutes.js
+++ routes/registerRoutes.js
@@ -1,10 +1,24 @@
+const path = require("path");
 const express = require("express");
 const registerController = require("../controllers/registerController.js");
 const router = express.Router();
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./public/images");
+  },
+  filename: (req, file, cb) => {
+    console.log(path.extname(file.originalname));
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
+
+const upload = multer({ storage });
 
 router.get("/", registerController.getRegister);
 
-// @POST - /register
-router.post('/', registerController.registerUser);
-
 module.exports = router;
+
+// @POST - /register
+router.post("/", upload.any("image"), registerController.registerUser);
