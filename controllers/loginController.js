7d0cb4e04f118d7e491fb35d47c12138a33baf35
TomasGM <tomasgonzalezm99@gmail.com>
2023-07-08 18:16:21 -0300

    Login

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -5,8 +5,10 @@ const userModel = require('../models/users');
 const controller = {
 
   getLogin: (req, res) => {
+    const error = req.query.error; // Obtener el valor del parámetro "error"
     res.render('login', {
-      title: 'Login'
+      title: 'Login',
+      error: error
     });
   },
 
@@ -16,21 +18,19 @@ const controller = {
     if (!searchedUser) {
       return res.redirect('/login?error=El mail o la contraseña son incorrectos');
     }
-    return res.send("Boca juniors");
-    
+
     const {
       password: hashedPw
     } = searchedUser;
     const isCorrect = bcrypt.compareSync(req.body.password, hashedPw);
 
     if (isCorrect) {
-      /*
+      
       if (!!req.body.remember) {
         res.cookie('email', searchedUser.email, {
           maxAge: 1000 * 60 * 60 * 24 * 360 * 9999
         });
       }
-      */
 
       delete searchedUser.password;
       delete searchedUser.id;
@@ -38,6 +38,7 @@ const controller = {
       req.session.user = searchedUser;
 
       res.redirect('/');
+      
     } else {
       return res.redirect('/login?error=El mail o la contraseña son incorrectos');
     }
