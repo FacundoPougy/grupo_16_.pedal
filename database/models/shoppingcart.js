17d3d9386b85036f34d3136170183aca0575a6b6
TomasGM <tomasgonzalezm99@gmail.com>
2023-07-30 20:00:20 -0300

    Models

--- database/models/shoppingcart.js
+++ database/models/shoppingcart.js
@@ -1,40 +1,35 @@
 module.exports = (sequelize, DataType) => {
-  const alias = 'ShoppingCart';
+    const alias = 'ShoppingCart';
 
-  const cols = {
-      // id: {
-      //     type: DataType.INTEGER,
-      //     primaryKey: true,
-      //     autoIncrement: true
-      // },
-      user_id: {
-          type: DataType.INTEGER,
-          allowNull: false,
-          references: {
-              model: 'users',
-              key: 'id'
-          }
-      },
-      product_id: {
-          type: DataType.INTEGER,
-          allowNull: false,
-          references: {
-              model: 'products',
-              key: 'id'
-          }
-      },
-      quantity: {
-          type: DataType.INTEGER,
-          allowNull: false
-      }
-  };
+    const cols = {
+        user_id: {
+            type: DataType.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'users',
+                key: 'id'
+            }
+        },
+        item_id: {
+            type: DataType.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'items',
+                key: 'id'
+            }
+        },
+        quantity: {
+            type: DataType.INTEGER,
+            allowNull: false
+        }
+    };
 
-  const config = {
-      tableName: 'shopping_cart',
-      timestamps: false
-  };
+    const config = {
+        tableName: 'shopping_cart',
+        timestamps: false
+    };
 
-  const ShoppingCart = sequelize.define(alias, cols, config);
+    const ShoppingCart = sequelize.define(alias, cols, config);
 
-  return ShoppingCart;
+    return ShoppingCart;
 };
