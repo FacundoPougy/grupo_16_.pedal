5c3af30ed300b034f1148b2b7dfe96d6c710611c
FacundoPougy <facu.pougy@gmail.com>
2023-08-09 19:40:29 -0300

    empezando crud de detalle del producto

--- controllers/productoController.js
+++ controllers/productoController.js
@@ -1,12 +1,14 @@
 const path = require("path");
 
-const { Product } = require('../database/models');
+const { Product } = require("../database/models");
+const { Item } = require("../database/models");
+const { log } = require("console");
 
 const controller = {
   // @GET /products
   getProductos: async (req, res) => {
-    const productos =  await Product.findAll();
-    console.log( productos);
+    const productos = await Product.findAll();
+    console.log(productos);
     let userData = req.session.user;
 
     res.render("products", {
@@ -18,13 +20,15 @@ const controller = {
 
   // @GET /products/:id/detail
   getProductoDetalle: async (req, res) => {
-    const productos =  await Product.findAll();
+    const productos = await Product.findAll();
 
     // Agarramos el ID que nos pasaron por parámetro de ruta, y lo convertimos en number
     const id = Number(req.params.id);
 
     // Buscamos en el array de productos, el producto cuyo ID coincida con el que nos enviaron por params
-    const productoAMostrar = await Product.findByPk(id);
+    const productoAMostrar = await Product.findByPk(id, {
+      include: "items",
+    });
 
     // Si el producto no se encuentra (su id es inválido)
     if (!productoAMostrar) {
