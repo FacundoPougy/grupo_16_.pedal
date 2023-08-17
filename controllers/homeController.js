ea8f4a4ad5506bf4fc6e8e33beb960fb8a8e06bf
FacundoPougy <facu.pougy@gmail.com>
2023-08-16 22:51:42 -0300

    search de productos

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -12,13 +12,16 @@ const controller = {
     });
   },
   getSearch: async (req, res) => {
+    /* Obtenemos el nombre del producto */
     const query = req.query.search;
 
+    /* Comparamos el nombre requerido por el cliente, con los de nuestra base de datos */
     let searchedProduct = await Product.findOne({
       where: {
         name: { [Op.like]: query },
       },
     });
+    /* Si se encuentra un resultado, se redirige al cliente al detalle del producto requerido */
     if (searchedProduct) {
       return res.redirect(`/productos/${searchedProduct.id}/detalle`);
     }
--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const homeController = require("../controllers/homeController.js");
 const router = express.Router();
 
+/* --GET-- */
 router.get("/", homeController.getIndex);
 
 router.get("/search", homeController.getSearch);
