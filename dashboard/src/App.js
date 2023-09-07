0d06fe0109fe1a904dc9d444347bdd3cfe1f59d6
TomasGM <tomasgonzalezm99@gmail.com>
2023-09-07 13:12:40 -0300

    mejoras esteticas y routeo

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -3,20 +3,22 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import TotalsPanel from './components/TotalsPanel';
 import Header from './components/Header';
+import WrapperComponent from './components/WrapperComponent';
+import NotFound from './components/NotFound';
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Header logoImage= "./Logo.jpg"/>
-        
-        <TotalsPanel/> 
-        <Switch>
-          {/* <Route exact path="/" component={Home} />
-          <Route path="/about" component={About} />
-          <Route path="/contact" component={Contact} />
-          <Route component={NotFound} /> */}
+        <WrapperComponent>
+          <Switch>
+          <Route exact path="/" component={TotalsPanel}/>
+          <Route exact path="/about" component={Header} />
+          <Route exact path="/contact" component={WrapperComponent} />
+          <Route component={NotFound} /> 
         </Switch>
+        </WrapperComponent>
       </div>
     </Router>
   );
