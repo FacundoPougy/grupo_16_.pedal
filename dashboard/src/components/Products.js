7277d04c8839cf0816693e921f03bafac1244b4d
TomasGM <tomasgonzalezm99@gmail.com>
2023-09-09 12:42:43 -0300

    Mejora a productos dash

--- dashboard/src/components/Products.js
+++ dashboard/src/components/Products.js
@@ -1,6 +1,48 @@
 import React, { useEffect, useState } from 'react';
 import '../static/css/Products.css';
 
+
+const SingleItem = ({item}) => {
+  return (
+  <div className='item-single'>
+    <img src={"http://localhost:3000/"+item.image} alt={"Item_"+item.id} />
+  </div>
+  );
+}
+
+const SingleProduct = ({product}) => {
+  return (
+    <article className='product-single'>
+
+      <div className='titles'>
+        <div className='product-title'>
+          <h3>{product.name}</h3>
+          <p>{product.description}</p>
+        </div>
+
+        <div className='item-title'>
+          <h3>Items</h3>
+          <p>Stock total: </p>
+        </div>
+      </div>
+
+      <div className='togle-container'>
+        <div className='product-complete'>
+          <img src={"http://localhost:3000/"+product.main_image} alt={"Image_"+product.name} />
+        </div>
+
+        <div className='items-complete'>
+          {product.items.map((item) => (
+            <SingleItem key={item.id} item={item}></SingleItem>
+          ))}
+        </div>
+      </div>
+
+    </article>
+  );
+}
+
+
 const Products = () => {
   const [products, setProducts] = useState([]);
 
@@ -24,19 +66,14 @@ const Products = () => {
   }, []);
 
   return (
-    <div className="products-container">
+    <section className='products-section'>
       <h2>Productos</h2>
-      <ul>
+    <div className="products-container">
         {products.map((product) => (
-          <li key={product.id}>
-            <h3>{product.name}</h3>
-            <p>{product.description}</p>
-            <p>Stock: {product.items.reduce((total, item) => total + item.stock, 0)}</p>
-            <img src={product.items[0].image} alt={product.name} />
-          </li>
-        ))}
-      </ul>
+          <SingleProduct key={product.id} product={product}></SingleProduct>
+          ))}
     </div>
+    </section>
   );
 };
 
