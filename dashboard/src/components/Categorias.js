4dbf76a698329f70823857de83320a112edccfeb
TomasGM <tomasgonzalezm99@gmail.com>
2023-09-10 23:42:47 -0300

    Categorias completo

--- dashboard/src/components/Categorias.js
+++ dashboard/src/components/Categorias.js
@@ -1,15 +1,57 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFolder } from '@fortawesome/free-solid-svg-icons';
+import Bars from 'react-bars';
 import '../static/css/Categorias.css'
+
 
 const Categorias = () => {
 
-  const estilo = {
-    color: 'white',
-  };
+  const [countByCategory, setcountByCategory] = useState([]);
+
+  const isMounted = useRef(true);
+
+  useEffect(() => {
+    
+    async function fetchData() {
+      try {
+        if (isMounted.current) {
+          const response = await fetch('http://localhost:3000/api/products');
+        
+          const {countByCategory} = await response.json();
+
+          const total = Object.values(countByCategory).reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+          
+          const transformedData = Object.keys(countByCategory).map((category) => ({
+            label: category+"s:"+countByCategory[category],
+            value: countByCategory[category],
+            barColor:'var(--naranja)',
+            maxValue:total,
+            barBackgroundColor:'var(--blanquito)',
+          }));
+
+          setcountByCategory(transformedData);
+        }
+      } catch (error) {
+        console.error('Error al obtener datos:', error);
+      }
+    }
+    
+    fetchData();
+
+    return () => {
+      isMounted.current = false;
+    };
+  }, []);
 
   return (
     <div className="categoria-container">
-      <p style={estilo}>Categorias</p>
+      <div className='title-categoria'>
+        <p> <FontAwesomeIcon icon={faFolder} />  Categorias</p>
+      </div>
+    <div className="bars-container">
+      <Bars data={countByCategory} />
+    </div>
     </div>
   );
 };
